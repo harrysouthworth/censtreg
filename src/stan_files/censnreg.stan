acfcb5df4c2ad9acb9d3127dc210cdf245a2bf3d
Harry <harry@dataclarityconsulting.co.uk>
2020-06-02 14:31:08 +0100

    Switching to master to check that tests pass there before rerunning here

--- src/stan_files/censnreg.stan
+++ src/stan_files/censnreg.stan
@@ -5,22 +5,25 @@ data {
   matrix[N_obs, K] x_obs;
   matrix[N_cens, K] x_cens;
   real y_obs[N_obs];
-  real L[N_cens];
+  vector[N_cens] L;
   real sigma_params[2];
 }
+transformed data{
+  real maxL = max(L);
+}
 parameters {
-  real y_cens[N_cens];
-  for (i in 1:N_cens){
-    real<upper=L[i]> tmp;
-    tmp = y_cens[i];
-    y_cens[i] = tmp;
-  }
+  vector<upper=max(L)>[N_cens] y_tmp;
+
   //vector[N_cens]<upper=L> y_cens;
   vector[K] beta;
   real<lower=0> sigma;
 }
+transformed parameters{
+  vector[N_cens] y_cens = y_tmp + L - maxL;
+  //y_cens = y_cens - maxL;
+}
 model {
   sigma ~ lognormal(sigma_params[1], sigma_params[2]);
   y_obs ~ normal(x_obs * beta, sigma);
-  y_cens ~ normal(x_cens * beta, sigma);
+  y_tmp ~ normal(x_cens * beta, sigma);
 }
---
+++ tests/testthat/test-censnreg-varying.R
@@ -0,0 +1,47 @@
+library(censtreg)
+library(testthat)
+
+test_that("Left-censored regression, varying limits: posterior means match generating mechanism", {
+  set.seed(20200514)
+  
+  between <- function(x, lower, upper){
+    x < upper & x > lower
+  }
+  
+  m <- o <- list(); i <- 1
+  
+  for (i in 1:10){
+    a <- sample(-10:10, size = 1)
+    b <- runif(1, -.5, 1.5)
+    s <- abs(rnorm(1, 1, 4))
+    nu <- runif(1, 10, 100)
+    
+    x <- rt(1000, nu) * s
+    y <- a +  b * x + rt(1000, nu) * s
+    
+    th <- quantile(y, prob = runif(length(y), .05, .4))
+    
+    d <- data.frame(x = x, y = ifelse (y >= th, y / 2, th))
+    
+    m[[i]] <- censtreg(y ~ x, data = d, limit = th, silent = TRUE, nu = Inf)
+    
+    o[[i]] <- censtreg(y ~ x, data = d, limit = th, silent = TRUE, nu = Inf,
+                       method = "integrate")
+    
+    sm <- coef(summary(m[[i]], probs = c(.01, .99)))
+    lo <- sm[, 4]
+    hi <- sm[, 5]
+    
+    truth <- c(a, b, s, nu)
+    
+    testthat::expect_true(all(between(truth, lo, hi)),
+                          label = "Coefficients look ok: left censored")
+    
+    sm <- coef(summary(o[[i]], probs = c(.01, .99)))
+    lo <- sm[, 4]
+    hi <- sm[, 5]
+    
+    testthat::expect_true(all(between(truth, lo, hi)),
+                          label = "Coefficients look ok: left censored")
+  }
+})
